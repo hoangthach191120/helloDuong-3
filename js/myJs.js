d9f5d77b22baed1a805a07146717879a7ad44e4f
hoangthach191120 <92820714+hoangthach191120@users.noreply.github.com>
2021-10-20 12:11:53 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luuuuuuuuuuu cậu!",
+  text1: "He luuu Dương!",
   text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
+  text3: "Dương nhớ tớ lắm có phải không nào ._.",
+  text4: "Nếu cậu ko trả lời mà thoát ra tức là rất rất nhớ và iu tớ đó nha :v",
   text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
+  text6: "Nhớ ơi là nhớ <3",
+  text7: "Lý do cậu thích tớ đi :vvvv",
   text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text9: "Vì cậu đẹp trai, tốt bụng nhất quả đất này hí hí",
+  text10: "Tớ biết mà ^^ ahihi",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Chúc cậu ngày 20/10 vui vẻ và luôn ngập tràn hạnh phúc nhé. Cho tớ gửi lời chúc đến mẹ cậu nữa nha <3 :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
   text12: "Okii lunn <3",
 };
 
